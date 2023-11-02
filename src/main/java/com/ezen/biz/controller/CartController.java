c807063d0d8a43efa216619e5844bbd9918630cd
CHOQWER <ctg2521111@gmail.com>
2023-11-02 18:25:41 +0900

    ㅈㅈ

--- src/main/java/com/ezen/biz/controller/CartController.java
+++ src/main/java/com/ezen/biz/controller/CartController.java
@@ -3,23 +3,16 @@ package com.ezen.biz.controller;
 
 import java.util.List;
 
-
 import javax.servlet.http.HttpSession;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 import com.ezen.biz.dto.CartVO;
-
 import com.ezen.biz.dto.ProductVO;
 import com.ezen.biz.dto.UsersVO;
 import com.ezen.biz.service.CartService;
--- src/test/java/com/ezen/biz/NoticeTest.java
+++ src/test/java/com/ezen/biz/NoticeTest.java
@@ -1,12 +1,10 @@
 package com.ezen.biz;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.ezen.biz.dto.NoticeVO;
 import com.ezen.biz.service.NoticeService;
 
 import lombok.extern.log4j.Log4j;
@@ -18,43 +16,34 @@ public class NoticeTest {
 	@Autowired
 	private NoticeService dao;
 	
-	@Test
-	public void insertNotice() {
-		NoticeVO vo=new NoticeVO();
-		vo.setN_title("공지사항6");
-		vo.setN_content("쿠팡공지사항6");
-		vo.setType("0");
-		dao.insertNotice(vo);
-	}
+//	@Test
+//	public void insertNotice() {
+//		NoticeVO vo=new NoticeVO();
+//		vo.setN_title("공지사항6");
+//		vo.setN_content("쿠팡공지사항6");
+//		dao.insertNotice(vo);
+//	}
+//	
+//	@Test
+//	public void updateNotice() {
+//		NoticeVO vo=new NoticeVO();
+//		vo.setN_title("공지사항6-1");
+//		vo.setN_content("쿠팡공지사항6-1");
+//		vo.setNno(12);
+//		dao.updateNotice(nno);
+//	}
 	
-	@Test
-	public void updateNotice() {
-		NoticeVO vo=new NoticeVO();
-		vo.setN_title("공지사항6-1");
-		vo.setN_content("쿠팡공지사항6-1");
-		vo.setNno(12);
-		dao.updateNotice(vo);
-	}
-	
-	@Test
-	public void deleteNotice() {
-		NoticeVO vo=new NoticeVO();
-		vo.setNno(12);
-		dao.deleteNotice(vo);
-	}
-	
-	@Test
-	public void selectNoticeList() {
-		NoticeVO vo=new NoticeVO();
-		vo.setType("0");
-		log.info(vo);
-		dao.selectNoticeList(vo);
-	}
-	
-	@Test
-	public void getNoticeList() {
-		NoticeVO vo=new NoticeVO();
-		vo.setSearchKeyword("쿠팡");
-		dao.getNoticeList(vo);
-	}
+//	@Test
+//	public void deleteNotice() {
+//		NoticeVO vo=new NoticeVO();
+//		vo.setNno(12);
+//		dao.deleteNotice(nno);
+//	}
+//	
+//	@Test
+//	public void selectNoticeList() {
+//		NoticeVO vo=new NoticeVO();
+//		log.info(vo);
+//		dao.selectNoticeList(vo);
+//	}
 }
